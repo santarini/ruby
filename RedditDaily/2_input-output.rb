7b770616b33c326a99920b2e9090a4d5e0268727
Makoa <32736507+santarini@users.noreply.github.com>
2017-12-12 23:31:26 -1000

    Create 2_input-output.rb

---
+++ RedditDaily/2_input-output.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "Where do you live?"
+location = gets.chomp
+puts "Your name is " + name + ", you are " + age + " years old, and you live in " + location + "."
